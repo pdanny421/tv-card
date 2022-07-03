cb0a7c16bb5f8777665d9dfc5a7ade70291363b4
pdanny421 <pdanny421@gmail.com>
2022-07-03 17:59:27 -0400

    Update tv-card.js

--- tv-card.js
+++ tv-card.js
@@ -364,7 +364,7 @@ class TVCardServices extends LitElement {
                 toucharea {
                     border-radius: 30px;
                     flex-grow: 1;
-                    height: 350px;
+                    height: 400px;
                     background: #6d767e;
                     touch-action: none;
                     text-align: center;
